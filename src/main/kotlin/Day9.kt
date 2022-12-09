a4bec0f2b987ed2459227cda0df6c42ca9e48218
Harm Kolk <github@sunstroke.nl>
2022-12-09 19:51:18 +0100

    Minor cleanup

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,35 +1,8 @@
 class Day9(val input: List<String>) {
-    data class Rope(val head: Point2D, val tail: Point2D)
-    fun solvePart1(): Int {
-        var head = Point2D(0, 0)
-        var tail = head
-        val visited = mutableListOf(tail)
-        input.forEach{
-            val parts = it.split(" ")
-            val direction = when(parts[0]) {
-                "U" -> Point2D.NORTH
-                "D" -> Point2D.SOUTH
-                "L" -> Point2D.WEST
-                "R" -> Point2D.EAST
-                else -> throw IllegalStateException()
-            }
-            for(i in 0 until parts[1].toInt()) {
-                val newHead = head.move(direction)
-                if(tail !in newHead.surrounding()) {
-                    // move the tail
-                    tail = head
-                    visited.add(tail)
-                }
-                head = newHead
-            }
-        }
-        visited.print()
-        return visited.toSet().size
-        TODO()
-    }
-
-    fun solvePart2(): Int {
-        var rope = (1..10).map { Point2D(0, 0) }
+    fun solvePart1() = solve(1)
+    fun solvePart2() = solve(9)
+    private fun solve(numKnots: Int): Int {
+        var rope = (0..numKnots).map { Point2D(0, 0) }
 
         val visited = mutableListOf(Point2D(0, 0))
         input.forEach{ line ->
@@ -45,8 +18,9 @@ class Day9(val input: List<String>) {
 
                 val newRope = mutableListOf<Point2D>()
                 newRope.add(rope[0].move(direction))
-                for(i in 1 .. 9) {
+                for(i in 1 .. numKnots) {
                     if(rope[i] in newRope[i-1].surrounding()) {
+                        // knot stays where it is
                         newRope.add(rope[i])
                     } else {
                         // move the knot
@@ -59,23 +33,18 @@ class Day9(val input: List<String>) {
                                 Point2D.DIRECTIONSDIAG.first { it(rope[i]) in newRope[i - 1].surrounding() }
                             newRope.add(newDirection(rope[i]))
                         }
-                        if(i == 9) {
+                        if(i == numKnots) {
                             visited.add(newRope[i])
                         }
                     }
                 }
                 rope = newRope
-                for(i in 1 ..9) {
+                for(i in 1 ..numKnots) {
                     if(rope[i] !in rope[i-1].surrounding()) {
                         println("ERROR !! $line - $moveCount")
                     }
                 }
-                //rope.print()
-                //println()
             }
-            //println(rope)
-            //visited.print()
-
         }
         return visited.toSet().size
     }
