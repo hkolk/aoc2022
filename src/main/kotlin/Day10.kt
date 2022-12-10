5a9b4a8227b71e10413d4c7594463bf6131239e8
Harm Kolk <github@sunstroke.nl>
2022-12-10 11:42:56 +0100

    Day10 cleanup

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,31 +1,29 @@
 class Day10(val input: List<String>) {
+
     fun solvePart1(): Int {
-        var round = 1
-        var register = 1
-        val history = mutableMapOf(round to register)
-        input.forEach { line ->
-            // execute
-            val parts = line.split(" ")
-            when(parts[0]) {
-                "addx" -> {
-                    history[round+1] = register
-                    register += parts[1].toInt()
-                    round += 2
-                    history[round] = register
-                }
-                "noop" -> {
-                    round += 1
-                    history[round] = register
-                }
-            }
-        }
-        return (20..220 step 40).map { history[it]!! * it}.sum()
-
-
-        TODO()
+        val history = runProgram()
+        return (20..220 step 40).sumOf { history[it]!! * it}
     }
 
-    fun runProgram(): Map<Int, Int> {
+
+    fun solvePart2(): Int {
+        val history = runProgram()
+        var drawn = 0
+        for(drawCycle in 1..240) {
+            if(history[drawCycle] in (drawCycle % 40)-2 .. (drawCycle % 40)) {
+                drawn++
+                print("░")
+            } else {
+                print(" ")
+            }
+            if(drawCycle % 40 == 0) {
+                println()
+            }
+        }
+        return drawn
+    }
+
+    private fun runProgram(): Map<Int, Int> {
         var round = 1
         var register = 1
         val history = mutableMapOf(round to register)
@@ -47,45 +45,4 @@ class Day10(val input: List<String>) {
         }
         return history
     }
-    fun solvePart2(): Int {
-        var round = 1
-        var register = 1
-        val history = mutableMapOf(round to register)
-        input.forEach { line ->
-            // execute
-            val parts = line.split(" ")
-            when(parts[0]) {
-                "addx" -> {
-                    history[round+1] = register
-                    register += parts[1].toInt()
-                    round += 2
-                    history[round] = register
-                }
-                "noop" -> {
-                    round += 1
-                    history[round] = register
-                }
-            }
-        }
-
-        var drawn = 0
-        for(drawCycle in 1..240) {
-            val value = history[drawCycle]
-            if(drawCycle == 10) {
-                //println("")
-                //println("$drawCycle, $value")
-            }
-            if(value in (drawCycle % 40)-2 .. (drawCycle % 40)) {
-                drawn++
-                print("░")
-            } else {
-                print(" ")
-            }
-            if(drawCycle % 40 == 0) {
-                println()
-            }
-        }
-        return drawn
-    }
-
 }
