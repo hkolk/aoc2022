c64057dd95d9aa9faa9f70f184f785da894162dc
Harm Kolk <github@sunstroke.nl>
2022-12-16 17:51:56 +0100

    Bloody mess

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -6,16 +6,22 @@ class Day16(val input: List<String>) {
     }
     private val start = "AA"
     private val timeLimit = 30
-    
-    fun pathTo(from: String, to: String, taken:List<String> = listOf()): List<String>? {
+
+    private val importantValves = valves.filter { it.value > 0 }.map { it.key } + start
+    private val shortestPaths = importantValves.flatMap { active ->
+        (importantValves - active).map { to ->
+            (active to to) to pathTo(active, to)!!.drop(1)
+        }
+    }.associate { it.first to it.second!! }
+
+    private fun pathTo(from: String, to: String, taken:List<String> = listOf()): List<String>? {
         if(from == to) { return taken + from }
         return tunnels[from]!!.filter { it !in taken }.mapNotNull {
             pathTo(it, to, taken + from)
         }.minByOrNull { it.size }
-        TODO()
     }
 
-    fun moveAndOpen(current: String, paths: Map<Pair<String, String>, List<String>>, timePassed: Int, opened: List<String>, flow: Int, released: Int): Pair<List<String>, Int> {
+    private fun moveAndOpen(current: String, paths: Map<Pair<String, String>, List<String>>, timePassed: Int, opened: List<String>, flow: Int, released: Int): Pair<List<String>, Int> {
         //val bestPath = listOf("AA", "DD", "BB", "JJ", "HH", "EE", "CC")
         val bestPath = listOf("VR", "SP", "RO", "KZ", "DI", "SO", "SC")
 
@@ -55,23 +61,91 @@ class Day16(val input: List<String>) {
         }
     }
 
-    fun solvePart1(): Int {
-        val importantValves = valves.filter { it.value > 0 }.map { it.key } + start
-        val shortestPaths = importantValves.flatMap { active ->
-            (importantValves - active).map { to ->
-                (active to to) to pathTo(active, to)!!.drop(1)
-            }
-        }.map{it.first to it.second!!}.toMap()
-        shortestPaths.forEach { path ->
-            println("${path.key} -> ${path.value}")
-        }
-        val max = moveAndOpen(start, shortestPaths, 0, listOf(), 0, 0)
-        println(max)
-        return max.second
+    data class Actor(val name: String, val destination: String, val timeToArrive: Int )
 
-        TODO()
+    fun tick(timePassed: Int, flow: Int, released: Int, human: Actor, elephant: Actor, opened: List<String>): Pair<List<String>, Int> {
+        val bestPath = listOf("AA", "AA", "DD", "JJ", "BB", "HH", "CC", "EE")
+        //val bestPath = listOf("VR", "SP", "RO", "KZ", "DI", "SO", "SC")
+
+        //println("${bestPath.take(opened.size)} == $opened = $debug")
+        // open the valve
+
+        if(timePassed >= timeLimit) {
+            return opened to released
+        }
+        // tick a minute off
+        val timePassed = timePassed + 1
+        val released = released + flow
+        val human = human.copy(timeToArrive = human.timeToArrive - 1)
+        val elephant = elephant.copy(timeToArrive = elephant.timeToArrive - 1)
+
+        var flow = flow
+        var opened = opened
+
+        // can anyone open yet?
+        if(human.timeToArrive == 0) {
+            flow += valves[human.destination]!!
+            opened = opened + human.destination
+        }
+        if(elephant.timeToArrive == 0) {
+            flow += valves[elephant.destination]!!
+            opened = opened + elephant.destination
+        }
+
+        val debug = (bestPath.take(opened.size) == opened)
+        if(debug) {
+            println("[$timePassed] opened is now $opened, human = $human, elephant = $elephant")
+        }
+
+        if(human.timeToArrive == -1) {
+            val options =
+                shortestPaths.filter { it.key.first == human.destination }.filter { it.key.second !in opened }
+            // if options is empty, just keep ticking
+            if (options.isEmpty()) {
+                tick(timePassed, flow, released, human, elephant, opened)
+            } else {
+                options.map { option ->
+                    val newHuman = human.copy(destination = option.key.second, timeToArrive = option.value.size)
+                    tick(timePassed, flow, released, newHuman, elephant, opened)
+                }.maxBy { it.second }
+            }
+        }
+        // can anyone move yet?
+        val movers = actors.filter { it.timeToArrive == -1 } // people that have arrived
+        if(movers.isEmpty()) {
+            // this universe survives. Just tick
+            return tick(timePassed, flow, released, actors, opened)
+        } else {
+            // all movers pick an option
+
+
+            val universes =  movers.map { actor ->
+                // options
+                val options =
+                    shortestPaths.filter { it.key.first == actor.destination }.filter { it.key.second !in opened }
+                // if options is empty, just keep ticking
+                if (options.isEmpty()) {
+                    tick(timePassed, flow, released, actors, opened)
+                } else {
+                    options.map { option ->
+                        val newActor = actor.copy(destination = option.key.second, timeToArrive = option.value.size)
+                        val newActors = actors.filter { it.name != newActor.name } + newActor
+                        tick(timePassed, flow, released, newActors, opened)
+                    }.maxBy { it.second }
+                }
+                // pick winner
+            }
+            return universes.maxBy { it.second }
+        }
+    }
+    fun solvePart1(): Int {
+        val max = moveAndOpen(start, shortestPaths, 0, listOf(), 0, 0)
+        return max.second
     }
     fun solvePart2(): Int {
+        val actors = listOf(Actor("myself", start, 1), Actor("elephant", start, 1))
+        val result = tick(3, 0, 0, actors, listOf())
+        println(result)
         TODO()
     }
 }
