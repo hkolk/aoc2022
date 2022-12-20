c4ca1fe7c733c3870334ac09e4f164e16d856330
Harm Kolk <github@sunstroke.nl>
2022-12-20 08:02:02 +0100

    Cleanup

--- src/main/kotlin/Day20.kt
+++ src/main/kotlin/Day20.kt
@@ -5,47 +5,30 @@ class Day20(input: List<String>) {
         override fun toString() = value.toString()
     }
     val input = input.mapIndexed() { idx, value -> Node(idx, value.toLong()) }
-    fun solvePart1(): Long {
-        val circle = MutableCircularList(input.toMutableList())
-        //println(circle)
-        for(i in input.indices) {
-            val idx = circle.indexOfFirst { it.origIdx == i }
-            val node = circle.removeAt(idx)
-            var newIdx = ((idx + node.value) % circle.size).toInt()
-            //println("[$i] $node has newIdx: $newIdx")
-            if(newIdx == 0) {
-                circle.add(node)
-            } else {
-                circle.add(newIdx, node)
 
-            }
-            //println(circle)
-        }
-        val zeroIdx = circle.indexOfFirst { it.value == 0L }
-        //println("${circle[1000+zeroIdx].value}+${circle[2000+zeroIdx].value}+${circle[3000+zeroIdx].value}")
-        return circle[1000+zeroIdx].value+circle[2000+zeroIdx].value+circle[3000+zeroIdx].value
-    }
-    fun solvePart2(): Long {
-        val circle = MutableCircularList(input.map {it.copy(value = Math.multiplyExact(it.value, 811_589_153)) }.toMutableList())
+    private fun solve(rounds:Int=1, decryptionKey:Long=1): Long {
+        val circle = MutableCircularList(input.map {
+            it.copy(value = Math.multiplyExact(it.value, decryptionKey))
+        }.toMutableList())
 
-        //println(circle)
-        for(round in 1..10) {
+        for(round in 1..rounds) {
             for (i in input.indices) {
                 val idx = circle.indexOfFirst { it.origIdx == i }
                 val node = circle.removeAt(idx)
                 var newIdx = ((idx + node.value) % circle.size).toInt()
-                //println("[$i] $node has newIdx: $newIdx")
                 if(newIdx == 0) {
                     circle.add(node)
                 } else {
                     circle.add(newIdx, node)
 
                 }
-                //println(circle)
             }
         }
         val zeroIdx = circle.indexOfFirst { it.value == 0L }
-        println("${circle[1000+zeroIdx].value}+${circle[2000+zeroIdx].value}+${circle[3000+zeroIdx].value}")
         return circle[1000+zeroIdx].value+circle[2000+zeroIdx].value+circle[3000+zeroIdx].value
     }
+
+    fun solvePart1() = solve()
+    fun solvePart2() = solve(10, 811_589_153)
+
 }
