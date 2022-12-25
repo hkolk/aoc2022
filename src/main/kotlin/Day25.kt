5ac5b53f23c3e6e63bd0d604ec4a80746c687897
Harm Kolk <github@sunstroke.nl>
2022-12-25 11:05:23 +0200

    Proper Snafu class

--- src/main/kotlin/Day25.kt
+++ src/main/kotlin/Day25.kt
@@ -2,17 +2,29 @@ import kotlin.IllegalStateException
 
 class Day25(val input: List<String>) {
 
-    private fun Int.pow(exponent: Long): Long {
-        return (1L..exponent).fold(1L){acc, _ -> acc * this}
-    }
-
-    private fun String.fromSnafuToLong(): Long {
-        return this.map { it.snafuDigitToInt() }.reversed().foldIndexed(0L) { idx, acc, it ->
-            (5).pow(idx.toLong()) * it+ acc
+    class SnafuNumber(val value: String) {
+        override fun toString() = value
+        private fun Int.pow(exponent: Long): Long {
+            return (1L..exponent).fold(1L){acc, _ -> acc * this}
+        }
+        fun toLong(): Long {
+            return this.value.map { it.snafuDigitToInt() }.reversed().foldIndexed(0L) { idx, acc, it ->
+                (5).pow(idx.toLong()) * it+ acc
+            }
+        }
+        private fun Char.snafuDigitToInt() = when(this) {
+            '=' -> -2
+            '-' -> -1
+            '0' -> 0
+            '1' -> 1
+            '2' -> 2
+            else -> throw IllegalStateException("$this is not a snafu digit")
         }
     }
 
-    private fun Long.toSnafu(): String {
+    private fun String.toSnafu() = SnafuNumber(this)
+
+    private fun Long.toSnafu(): SnafuNumber {
         var acc = 0
         return this.toString(5).reversed().map { c ->
             when(c.digitToInt()+acc) {
@@ -24,18 +36,11 @@ class Day25(val input: List<String>) {
                 5 -> { acc = 1; '0' } // safe because the radix convert cannot return this
                 else -> throw IllegalStateException("Unknown snafu digit: $c")
             }
-        }.reversed().joinToString("")
+        }.reversed().joinToString("").toSnafu()
     }
 
-    private fun Char.snafuDigitToInt() = when(this) {
-        '=' -> -2
-        '-' -> -1
-        '0' -> 0
-        '1' -> 1
-        '2' -> 2
-        else -> throw IllegalStateException("$this is not a snafu digit")
-    }
-    fun solvePart1() = input.sumOf { it.fromSnafuToLong() }.toSnafu()
+
+    fun solvePart1() = input.sumOf { it.toSnafu().toLong() }.toSnafu().toString()
 
     fun solvePart2() = 1 // 1 free star
 }
